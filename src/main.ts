e99752833861a319e98b43df7899186553e573a2
Shuvrojit Biswas <jsuvro17@gmail.com>
2023-07-10 19:03:40 +0600

    Rework fileValidation
    Implemented the fileValidation in different
    way. It will file validate the file when
    compare button is clicked.

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,23 @@
 import './style.css'
 
-const yourFile = document.getElementById("your-file")
-const newFile = document.getElementById("new-file")
+const yourFileElement = document.getElementById("your-file")
+const newFileElement = document.getElementById("new-file")
 
 function fileValidation(file) {
-  if (!file) {
-    throw new Error("No file selected")
-  }
-  if (file.type !== "text/csv") {
-    throw new Error("Input a csv file")
-  }
+    if (!file) {
+        throw new Error("No file selected")
+    }
+    if (file.type !== "text/csv") {
+        throw new Error("Input a csv file")
+    }
 }
 
 const compareButton = document.getElementById("compare")
 compareButton?.addEventListener("click", () => {
-  const yfile = yourFile.files[0]
-  const nfile = newFile.files[0]
 
-  fileTypeValidation(yfile)
-  fileTypeValidation(nfile)
+    const yourFile = yourFileElement?.files[0]
+    const newFile = newFileElement?.files[0]
 
-  console.log(yfile)
-  console.log(nfile)
+    fileValidation(yourFile)
+    fileValidation(newFile)
 })
