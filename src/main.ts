a4fac066f8a5de96e0cbae22c467922c558e5505
Shuvrojit Biswas <jsuvro17@gmail.com>
2023-07-10 18:39:22 +0600

    Add fileValidation function
    fileValidation function will take a file
    and check if the file is valid and if the
    file type is text/csv.

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,22 @@ import './style.css'
 const yourFile = document.getElementById("your-file")
 const newFile = document.getElementById("new-file")
 
+function fileValidation(file) {
+  if (!file) {
+    throw new Error("No file selected")
+  }
+  if (file.type !== "text/csv") {
+    throw new Error("Input a csv file")
+  }
+}
+
 const compareButton = document.getElementById("compare")
 compareButton?.addEventListener("click", () => {
-  var yfile = yourFile.files[0]
-  var nfile = newFile.files[0]
+  const yfile = yourFile.files[0]
+  const nfile = newFile.files[0]
+
+  fileTypeValidation(yfile)
+  fileTypeValidation(nfile)
 
   console.log(yfile)
   console.log(nfile)
