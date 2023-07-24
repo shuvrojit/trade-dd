a79554630c30923a34406db02c288d66e0f32ec4
Shuvrojit Biswas <jsuvro17@gmail.com>
2023-07-24 17:50:46 +0600

    Implement extract data from csv file

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,30 @@
 import './style.css'
+import Papa from "papaparse"
 
-const yourFileElement = document.getElementById("your-file")
-const newFileElement = document.getElementById("new-file")
+const YOURDATA = []
 
 function fileValidation(file) {
     if (!file) {
         throw new Error("No file selected")
     }
-    if (file.type !== "text/csv") {
-        throw new Error("Input a csv file")
-    }
 }
 
+function getData(file) {
+    const reader = new FileReader()
+    reader.onload = (e) => {
+        const csvData = e.target.result
+        const parsedData = Papa.parse(csvData, { header: true })
+        for (let i in parsedData) {
+           YOURDATA.push(parsedData[i])
+        }
+    }
+    reader.readAsText(file)
+}
+
+
+
+const yourFileElement = document.getElementById("your-file")
+const newFileElement = document.getElementById("new-file")
 const compareButton = document.getElementById("compare")
 compareButton?.addEventListener("click", () => {
 
@@ -20,4 +33,9 @@ compareButton?.addEventListener("click", () => {
 
     fileValidation(yourFile)
     fileValidation(newFile)
+    getData(yourFile)
+    getData(newFile)
+    console.log(YOURDATA[0])
+    console.log(YOURDATA[3])
+
 })
